ad9ed148c51dc76bf3dda2aedc06fc1f2b01817f
Fluxid <fluxid@o2.pl>
2008-07-27 23:42:02 +0200

    działa

--- mocp-scrobbler.py
+++ mocp-scrobbler.py
@@ -9,7 +9,8 @@ import time
 import md5
 import re
 
-_CLIENTNAME = 'tst'
+_CLIENTNAME = 'tst' # Oficjalny, testowy
+#_CLIENTNAME = 'mcl' # Mój własny, oficjalny
 _CLIENTVER = '1.0'
 
 _USER = 'fluxid'
@@ -85,65 +86,131 @@ def get_mocp():
 	for line in mocp:
 		if test.match(line):
 			x = line.split(': ', 1)
-			info[x[0]] = x[1].strip()
+			info[x[0].lower()] = x[1].strip()
 	mocp.close()
 	return info
 
 def now_playing(track):
-	send_encoded(np_link, {'s': session, 'a': track['Artist'], 't': track['SongTitle'], 'b': track['Album'], 'l': track['TotalSec'], 'n': '', 'm': ''})
+	send_encoded(np_link, {'s': session, 'a': track['artist'], 't': track['songtitle'], 'b': track['album'], 'l': track['totalsec'], 'n': '', 'm': ''})
 
 def scrobble(track):
-	'Scrobbled!'
-	send_encoded(sub_link, {'s': session, 'a[0]': track['Artist'], 't[0]': track['SongTitle'], 'i[0]': int(time.time()), 'o[0]': 'P', 'r[0]': '', 'l[0]': track['TotalSec'], 'b[0]': track['Album'], 'n[0]': '', 'm[0]': ''})
+	send_encoded(sub_link, {'s': session, 'a[0]': track['artist'], 't[0]': track['songtitle'], 'i[0]': int(time.time()), 'o[0]': 'P', 'r[0]': '', 'l[0]': track['totalsec'], 'b[0]': track['album'], 'n[0]': '', 'm[0]': ''})
 
-def diff(track1, track2):
-	return not ( (track1['Artist'].lower() == track2['Artist'].lower()) and (track1['SongTitle'].lower() == track2['SongTitle'].lower()) )
+def diff(oldtrack, newtrack):
+	if newtrack and not oldtrack:
+		return True
+	if not ('artist' in oldtrack and 'artist' in newtrack and 'songtitle' in oldtrack and 'songtitle' in newtrack):
+		return False
+	return not ( (oldtrack['artist'].lower() == newtrack['artist'].lower()) and (oldtrack['songtitle'].lower() == newtrack['songtitle'].lower()) )
 
-def can_scrobble(track):
-	return ((int(track['TotalSec']) >= 240) and (int(track['CurrentSec']) > (float(track['TotalSec']) * _SCROB_FRAC)))
+def can_scrobble(track, frac):
+	if not track:
+		return False
+	if 'totalsec' in track:
+		total = float(track['totalsec'])
+	else:
+		total = 0;
+	if total <= 30: return False
+	if 'currentsec' in track:
+		current = int(track['currentsec'])
+	else:
+		current = 0;
+	
+	return (current > total * frac)
 
-def can_scrobble2(track):
-	return ((int(track['TotalSec']) >= 240) and (int(track['CurrentSec']) > (float(track['TotalSec']) * 0.5)))
+def can_scrobble_min(track):
+	if not track:
+		return False
+	if 'totalsec' in track:
+		total = float(track['totalsec'])
+	else:
+		total = 0;
+	if total <= 30: return False
+	if 'currentsec' in track:
+		current = int(track['currentsec'])
+	else:
+		current = 0;
+	return (current > 240) or (current > total * 0.5)
 
 def main():
-	authorize()
+	try:
+		authorize()
+	except BannedException:
+		print 'Error while authorizing: your account is banned.'
+		return
+	except BadAuthException:
+		print 'Error while authorizing: incorrect username or password. Please check your login settings and try again.'
+		return
+	except BadTimeException:
+		print 'Error while authorizing: incorrect time setting. Please check your clock settings and try again.'
+		return
+	except FailedException, e:
+		print 'Error while authorizing: general failure. Reason: "%s"' % e.message
+		return
+	except HardErrorException:
+		print 'Critical error while authorizing. Check your internet connection and try again. Maybe servers are dead?'
+		return
+	
+	print 'Authorized'
+	
 	unscrobbled = True
 	unnotified = True
 	
-	newtrack = get_mocp()
-	oldtrack = newtrack
+	newtrack = None
+	oldtrack = None
+	
+	maxsec = 0
+	
 	while True:
 		try:
 			while True:
-				if newtrack['State'] == 'PLAY':
-					if diff(oldtrack, newtrack):
-						print 'Changed track'
-						if unscrobbled and can_scrobble2(oldtrack):
-							print 'Scrobbled [Change]'
+				newtrack = get_mocp()
+				if 'state' in newtrack and newtrack['state'].lower() == 'play':
+					a = diff(oldtrack, newtrack)
+					b = not a and (int(newtrack['totalsec']) - 15 < maxsec) and (int(newtrack['currentsec']) < 15)
+					if a or b:
+						if oldtrack: oldtrack['currentsec'] = maxsec
+						if unscrobbled and can_scrobble_min(oldtrack):
+							print 'Scrobbled [on change]'
 							scrobble(oldtrack)
+						
+						if b:
+							print 'Now playing (repeated): %s - %s (%s)' % (newtrack['songtitle'], newtrack['artist'], newtrack['album'])
+						else:
+							print 'Now playing: %s - %s (%s)' % (newtrack['songtitle'], newtrack['artist'], newtrack['album'])
+						
 						oldtrack = newtrack
 						unscrobbled = True
 						unnotified = True
+						maxsec = 0
+					
+					maxsec = max(maxsec, int(newtrack['currentsec']))
 					
 					if unnotified:
-						print 'Notified'
 						now_playing(newtrack)
+						print 'Notify sent.'
 						unnotified = False
-			
-					if unscrobbled and can_scrobble(newtrack):
-						print 'Scrobbled [90%]'
+					
+					if unscrobbled and can_scrobble(newtrack, _SCROB_FRAC):
+						print 'Scrobbled [on %d%%]' % int(_SCROB_FRAC * 100)
 						scrobble(newtrack)
 						unscrobbled = False
+					
 				time.sleep(2)
-				newtrack = get_mocp()
 		except BadSessionException:
 			authorize()
-		except KeyError:
-			pass
+		except FailedException, e:
+			print 'Error while submission: general failure. Reason: "%s". Continuing after 5 seconds (scrobbling cache is unimplemented yet)' % e.message
+			time.sleep(5)
+		except HardErrorException:
+			print 'Critical error while submission. Check your internet connection. Trying again after 10 seconds (scrobbling cache is unimplemented yet)'
+			time.sleep(10)
 		except KeyboardInterrupt:
 			break
-		else:
-			pass
+#		except Exception, e:
+#			print e
+#		time.sleep(5)
+#		print 'Continuing'
 
 #	print session, np_link, sub_link
 #	try:
